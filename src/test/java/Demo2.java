8c9e15d029c79ffda55c3141dbfe01cb9b8c74ee
snehbharti <snehbharti5@gmail.com>
2025-04-19 10:27:51 +0530

    added surefile plugin in pom & configure testng.xml in pom

--- src/test/java/Demo2.java
+++ src/test/java/Demo2.java
@@ -1,11 +1,9 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
-public class Demo {
-
+public class Demo2 {
+	
 	public static void main(String[] args) {
 		String s = "My name is sneh bharti ,I Am foodiE, aa A I have umbrella";
 		char[] ch = s.toCharArray();
@@ -37,19 +35,11 @@ public class Demo {
 
 		}
 		System.out.println(hm);
-
 		ArrayList<Map.Entry<Character, Integer>> al = new ArrayList<>(hm.entrySet());
 
-		al.sort(Map.Entry.comparingByValue());
-		System.out.println((al)); // [A=1, E=1, u=1, I=2, o=2, i=3, a=4, e=4] ----->a. Answer in arralist
-
-		LinkedHashMap<Character, Integer> hmFinal = new LinkedHashMap<>();
-
-		for (Entry<Character, Integer> a : al) {
-			hmFinal.put(a.getKey(), a.getValue());
-		}
-		System.out.println(hmFinal);// {A=1, E=1, u=1, I=2, o=2, i=3, a=4, e=4} ------> b. Answer in hashmap
-
+		al.sort(Map.Entry.comparingByKey());
+		
+		System.out.println((al));
 	}
 
 }
