800e947fa480aade439631e6776103d027fa3ac8
snehbharti <snehbharti5@gmail.com>
2025-04-19 12:35:42 +0530

    test case added in testng.xml

---
+++ src/test/java/Test2.java
@@ -0,0 +1,8 @@
+
+public class Test2 {
+	@org.testng.annotations.Test
+	public void m1() {
+		System.out.println("test 2");
+	}
+
+}
---
+++ src/test/java/Test3.java
@@ -0,0 +1,8 @@
+
+public class Test3 {
+	@org.testng.annotations.Test
+	public void m1() {
+		System.out.println("test3");
+	}
+
+}
