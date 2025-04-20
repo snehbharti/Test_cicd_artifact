47ec21446da2df0709abc4d566bdaef59a26856f
snehbharti <snehbharti5@gmail.com>
2025-04-20 13:12:55 +0530

    f

--- src/test/java/Properties_Test.java
+++ src/test/java/Properties_Test.java
@@ -1,20 +1,53 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 public class Properties_Test {
 
+//	@BeforeSuite
+//	@Parameters("env")
+//	public void o(@Optional("qa")  String s) {
+//		WebDriver driver = new ChromeDriver();
+//		String env = System.getProperty("env", "qa"); // default to QA
+//		System.out.println("Environment: " + env);
+//		ConfigLoader config = new ConfigLoader(env);
+//		driver.get(config.get("base.url"));
+//		System.out.println(config.get("username"));
+//
+//	}
+	/*
+	 * @BeforeSuite
+	 * 
+	 * @Parameters("env") public void setup(@Optional("qa") String envFromXml) { //
+	 * Priority: System property > TestNG XML > default "qa" String finalEnv =
+	 * System.getProperty("env") != null ? System.getProperty("env") : (envFromXml
+	 * != null ? envFromXml : "qa");
+	 * 
+	 * System.out.println("Environment: " + finalEnv);
+	 * 
+	 * ConfigLoader config = new ConfigLoader(finalEnv);
+	 * 
+	 * WebDriver driver = new ChromeDriver(); driver.get(config.get("base.url"));
+	 * 
+	 * System.out.println("Username from config: " + config.get("username")); }
+	 */
 	@BeforeSuite
 	@Parameters("env")
-	public void o(String s) {
-		WebDriver driver = new ChromeDriver();
-		String env = System.getProperty("env", "qa"); // default to QA
-		System.out.println("Environment: " + env);
-		ConfigLoader config = new ConfigLoader(env);
-		driver.get(config.get("base.url"));
-		System.out.println(config.get("username"));
+	public void setup(@Optional("qa") String envFromXml) {
+		// Priority: system property > testng.xml param > default "qa"
+		String finalEnv = System.getProperty("env") != null ? System.getProperty("env")
+				: (envFromXml != null ? envFromXml : "qa");
 
+		System.out.println("Environment: " + finalEnv);
+
+		ConfigLoader config = new ConfigLoader(finalEnv);
+
+		WebDriver driver = new ChromeDriver();
+		driver.get(config.get("base.url"));
+
+		System.out.println("Username from config: " + config.get("username"));
 	}
 
 }
--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,5 +1,5 @@
 
-public class Test {
+public class Test1 {
 	@org.testng.annotations.Test
 	public void m1() {
 		System.out.println("test");
