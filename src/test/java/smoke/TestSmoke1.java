37a3c31ac4d195fc869c891f599e0e5448e24a62
snehbharti <snehbharti5@gmail.com>
2025-06-21 22:24:43 +0530

    smoke xml setup in jenkin

---
+++ src/test/java/smoke/TestSmoke1.java
@@ -0,0 +1,10 @@
+package smoke;
+
+public class TestSmoke1 {
+
+	@org.testng.annotations.Test
+	public void m1() {
+		System.out.println("test 5");
+	}
+
+}
---
+++ src/test/java/smoke/TestSmoke2.java
@@ -0,0 +1,5 @@
+package smoke;
+
+public class TestSmoke2 {
+
+}
