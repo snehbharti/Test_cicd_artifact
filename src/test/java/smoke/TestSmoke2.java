40058149cff423e95207a91cf673b64beeb393dc
snehbharti <snehbharti5@gmail.com>
2025-06-22 07:39:53 +0530

    Added TestSmoke class to test facebook title in jenkins

--- src/test/java/smoke/TestSmoke2.java
+++ src/test/java/smoke/TestSmoke2.java
@@ -1,5 +1,17 @@
 package smoke;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class TestSmoke2 {
+	
+	@Test
+	public void getFacebookTitle() {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://facebook.com");
+		Reporter.log(driver.getTitle());
+		
+	}
 
 }
