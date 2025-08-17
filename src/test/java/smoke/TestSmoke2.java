4258f1c78d103096b9b48c033c6b40f0e9d14f6d
snehbharti <135140896+snehbharti@users.noreply.github.com>
2025-08-17 16:19:58 +0530

    Update TestSmoke2.java

--- src/test/java/smoke/TestSmoke2.java
+++ src/test/java/smoke/TestSmoke2.java
@@ -12,17 +12,17 @@ public class TestSmoke2 {
 		System.out.println("Test Smoke 2 start");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless=new");  // Important for Chrome 109+
-		options.setBinary("/usr/bin/google-chrome-stable");
+		// options.setBinary("/usr/bin/google-chrome-stable");
 
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--disable-gpu");
-		options.addArguments("--window-size=1920x1080");
-		options.addArguments("--disable-notifications");
-		options.addArguments("--disable-blink-features=AutomationControlled");
-		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.178 Safari/537.36");
-		options.addArguments("--remote-allow-origins=*"); // Sometimes needed to avoid startup failure
-		options.addArguments("--remote-debugging-port=9222");
+		// options.addArguments("--no-sandbox");
+		// options.addArguments("--disable-dev-shm-usage");
+		// options.addArguments("--disable-gpu");
+		// options.addArguments("--window-size=1920x1080");
+		// options.addArguments("--disable-notifications");
+		// options.addArguments("--disable-blink-features=AutomationControlled");
+		// options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.178 Safari/537.36");
+		// options.addArguments("--remote-allow-origins=*"); // Sometimes needed to avoid startup failure
+		// options.addArguments("--remote-debugging-port=9222");
 
 		ChromeDriver driver=new ChromeDriver(options);
 		driver.get("https://facebook.com");
