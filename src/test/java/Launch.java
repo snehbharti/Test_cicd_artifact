1b08955c2c47e4032961ef46fec90fda351ad1a6
snehbharti <135140896+snehbharti@users.noreply.github.com>
2025-08-17 13:29:33 +0530

    Update Launch.java
    trying to run on aws

--- src/test/java/Launch.java
+++ src/test/java/Launch.java
@@ -14,21 +14,33 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class Launch {
 	public static void main(String[] args) throws AWTException, InterruptedException {
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--profile-directory=Default");
-		// Our systems have detected unusual traffic from your computer network. This
-		// page checks to see if it's really you sending the requests,
-		// and not a robot. Why did this happen?
-		options.addArguments("--disable-blink-features=AutomationControlled");
-		// Both of these options are commonly used to bypass automation detection in
-		// browsers when using Selenium WebDriver.
-		// Note- Use both
-		options.addArguments("--headless");
-		options.addArguments("--disable-gpu"); // Disable GPU usage to avoid crashes
+		// options.addArguments("--profile-directory=Default");
+		// // Our systems have detected unusual traffic from your computer network. This
+		// // page checks to see if it's really you sending the requests,
+		// // and not a robot. Why did this happen?
+		// options.addArguments("--disable-blink-features=AutomationControlled");
+		// // Both of these options are commonly used to bypass automation detection in
+		// // browsers when using Selenium WebDriver.
+		// // Note- Use both
+		// options.addArguments("--headless");
+		// options.addArguments("--disable-gpu"); // Disable GPU usage to avoid crashes
+		// options.addArguments("--window-size=1920x1080");
+		// options.addArguments("--disable-notifications");
+		// options.addArguments(
+		// 		"user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.178 Safari/537.36");
+		// // options.addArguments("--profile-directory=Default");
+
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");  // Important for Chrome 109+
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-gpu");
 		options.addArguments("--window-size=1920x1080");
 		options.addArguments("--disable-notifications");
-		options.addArguments(
-				"user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.178 Safari/537.36");
-		// options.addArguments("--profile-directory=Default");
+		options.addArguments("--disable-blink-features=AutomationControlled");
+		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.178 Safari/537.36");
+
+WebDriver driver = new ChromeDriver(options);
 
 		WebDriver driver = new ChromeDriver(options);
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
