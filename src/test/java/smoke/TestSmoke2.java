0e372e30329e052f294e09f604794545c2487856
snehbharti <135140896+snehbharti@users.noreply.github.com>
2025-08-17 16:07:23 +0530

    Update TestSmoke2.java

--- src/test/java/smoke/TestSmoke2.java
+++ src/test/java/smoke/TestSmoke2.java
@@ -20,6 +20,8 @@ public class TestSmoke2 {
 		options.addArguments("--disable-blink-features=AutomationControlled");
 		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.178 Safari/537.36");
 		options.addArguments("--remote-allow-origins=*"); // Sometimes needed to avoid startup failure
+		options.addArguments("--remote-debugging-port=9222");
+
 		ChromeDriver driver=new ChromeDriver(options);
 		driver.get("https://facebook.com");
 		Reporter.log(driver.getTitle(), true);
