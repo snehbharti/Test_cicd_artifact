bd5cceabf1e35ba4a764271739c02ce019ff5821
snehbharti <135140896+snehbharti@users.noreply.github.com>
2025-08-17 15:59:40 +0530

    Update TestSmoke2.java

--- src/test/java/smoke/TestSmoke2.java
+++ src/test/java/smoke/TestSmoke2.java
@@ -9,7 +9,17 @@ public class TestSmoke2 {
 	@Test
 	public void getFacebookTitle() {
 		System.out.println("Test Smoke 2 start");
-		ChromeDriver driver=new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new");  // Important for Chrome 109+
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--window-size=1920x1080");
+		options.addArguments("--disable-notifications");
+		options.addArguments("--disable-blink-features=AutomationControlled");
+		options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.6998.178 Safari/537.36");
+		options.addArguments("--remote-allow-origins=*"); // Sometimes needed to avoid startup failure
+		ChromeDriver driver=new ChromeDriver(options);
 		driver.get("https://facebook.com");
 		Reporter.log(driver.getTitle(), true);
 		System.out.println(driver.getTitle());
